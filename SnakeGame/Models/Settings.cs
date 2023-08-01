3c06f96d390cdb93dc3dc4914188fab17a3bc683
Alexander Nole <cnole_azabache@outlook.com>
2023-08-01 01:59:49 -0500

    first commit

---
+++ SnakeGame/Models/Settings.cs
@@ -0,0 +1,23 @@
+﻿using SnakeGame.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.Models
+{
+    public class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static Directions Directions { get; set; }
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            Directions = Directions.Left;
+        }
+    }
+}
