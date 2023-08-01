f5a36944a18d457c0255337490c84c0df7d1cddd
Alexander Nole <cnole_azabache@outlook.com>
2023-08-01 12:53:12 -0500

    Start game method

--- SnakeGame/Views/Form1.Designer.cs
+++ SnakeGame/Views/Form1.Designer.cs
@@ -28,22 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            lblHighScore = new Label();
+            lblScore = new Label();
+            canvas = new PictureBox();
+            btnStart = new Button();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)canvas).BeginInit();
             SuspendLayout();
+            // 
+            // lblHighScore
+            // 
+            lblHighScore.AutoSize = true;
+            lblHighScore.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblHighScore.Location = new Point(137, 13);
+            lblHighScore.Name = "lblHighScore";
+            lblHighScore.Size = new Size(103, 23);
+            lblHighScore.TabIndex = 7;
+            lblHighScore.Text = "High Score:";
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblScore.Location = new Point(12, 13);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(74, 23);
+            lblScore.TabIndex = 6;
+            lblScore.Text = "Score: 0";
+            // 
+            // canvas
+            // 
+            canvas.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            canvas.Location = new Point(12, 47);
+            canvas.Name = "canvas";
+            canvas.Size = new Size(567, 582);
+            canvas.TabIndex = 5;
+            canvas.TabStop = false;
+            canvas.Paint += UpdateCanvasGraphics;
+            // 
+            // btnStart
+            // 
+            btnStart.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnStart.BackColor = Color.Green;
+            btnStart.Cursor = Cursors.Hand;
+            btnStart.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnStart.ForeColor = SystemColors.ControlLightLight;
+            btnStart.Location = new Point(473, 8);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(106, 33);
+            btnStart.TabIndex = 4;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = false;
+            btnStart.Click += BtnStartClick;
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 120;
+            gameTimer.Tick += GameTimerTick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(591, 637);
+            Controls.Add(lblHighScore);
+            Controls.Add(lblScore);
+            Controls.Add(canvas);
+            Controls.Add(btnStart);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            KeyDown += KeysIsDown;
+            KeyUp += KeysIsUp;
+            ((System.ComponentModel.ISupportInitialize)canvas).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblHighScore;
+        private Label lblScore;
+        private PictureBox canvas;
+        private Button btnStart;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
--- SnakeGame/Views/Form1.cs
+++ SnakeGame/Views/Form1.cs
@@ -1,10 +1,200 @@
+using SnakeGame.Models;
+using SnakeGame.Common;
+
 namespace SnakeGame.Views
 {
     public partial class Form1 : Form
     {
+        private readonly List<Circle> _snake;
+        private readonly Random _rand;
+        private Circle _food;
+
+        private int _maxWidth;
+        private int _maxHeight;
+
+        private int _score;
+        private int _highScore;
+
+        private bool _isUp;
+        private bool _isDown;
+        private bool _isRight;
+        private bool _isLeft;
+
         public Form1()
         {
             InitializeComponent();
+            _ = new Settings();
+            _snake = new List<Circle>();
+            _rand = new Random();
+            _food = new Circle();
+        }
+        private void BtnStartClick(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void KeysIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && Settings.Directions != Directions.Right)
+                _isLeft = true;
+
+            if (e.KeyCode == Keys.Right && Settings.Directions != Directions.Left)
+                _isRight = true;
+
+            if (e.KeyCode == Keys.Up && Settings.Directions != Directions.Down)
+                _isUp = true;
+
+            if (e.KeyCode == Keys.Down && Settings.Directions != Directions.Up)
+                _isDown = true;
+        }
+
+        private void KeysIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                _isLeft = false;
+
+            if (e.KeyCode == Keys.Right)
+                _isRight = false;
+
+            if (e.KeyCode == Keys.Up)
+                _isUp = false;
+
+            if (e.KeyCode == Keys.Down)
+                _isDown = false;
+        }
+
+        private void UpdateCanvasGraphics(object sender, PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            Brush snakeColour;
+
+            for (int i = 0; i < _snake.Count; i++)
+            {
+                snakeColour = i == 0 ? Brushes.Green : Brushes.Black;
+                graphics.FillEllipse(snakeColour, new Rectangle
+                {
+                    X = _snake[i].X * Settings.Width,
+                    Y = _snake[i].Y * Settings.Height,
+                    Width = Settings.Width,
+                    Height = Settings.Height
+                });
+            }
+
+            graphics.FillEllipse(Brushes.Red, new Rectangle
+            {
+                X = _food.X * Settings.Width,
+                Y = _food.Y * Settings.Height,
+                Width = Settings.Width,
+                Height = Settings.Height
+            });
+        }
+
+        private void GameTimerTick(object sender, EventArgs e)
+        {
+            // Directions Configuration
+            if (_isLeft) Settings.Directions = Directions.Left;
+            if (_isRight) Settings.Directions = Directions.Right;
+            if (_isDown) Settings.Directions = Directions.Down;
+            if (_isUp) Settings.Directions = Directions.Up;
+
+            for (int i = _snake.Count - 1; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    _snake[i].X = _snake[i - 1].X;
+                    _snake[i].Y = _snake[i - 1].Y;
+                }
+                else
+                {
+                    switch (Settings.Directions)
+                    {
+                        case Directions.Left:
+                            _snake[i].X--;
+                            break;
+                        case Directions.Right:
+                            _snake[i].X++;
+                            break;
+                        case Directions.Up:
+                            _snake[i].Y--;
+                            break;
+                        case Directions.Down:
+                            _snake[i].Y++;
+                            break;
+                    }
+
+
+                }
+            }
+
+            canvas.Invalidate();
+        }
+
+        private void RestartGame()
+        {
+            // Initial Configuration
+            _maxWidth = canvas.Width / Settings.Width - 1;
+            _maxHeight = canvas.Height / Settings.Height - 1;
+            _score = 0;
+
+            _snake.Clear();
+
+            btnStart.Enabled = false;
+            lblScore.Text = $"Score: {_score}";
+            gameTimer.Interval = 120;
+
+            Settings.Directions = Directions.Left;
+            _isUp = _isDown = _isRight = _isLeft = false;
+
+            _snake.Add(new Circle { X = 10, Y = 5 }); // Head
+
+            for (int i = 0; i < 6; i++) // Body
+                _snake.Add(new Circle());
+
+            CreateFood();
+            gameTimer.Start();
+        }
+
+        private void EatFood()
+        {
+            _score++;
+            lblScore.Text = $"Score: {_score}";
+
+            var body = new Circle
+            {
+                X = _snake[_snake.Count - 1].X,
+                Y = _snake[_snake.Count - 1].Y
+            };
+
+            _snake.Add(body);
+
+            if (gameTimer.Interval > 60) gameTimer.Interval -= 3;
+            else if (gameTimer.Interval > 40) gameTimer.Interval--;
+            else gameTimer.Interval = 40;
+
+            CreateFood();
+        }
+
+        private void CreateFood()
+        {
+            _food = new Circle
+            {
+                X = _rand.Next(2, _maxWidth),
+                Y = _rand.Next(2, _maxHeight)
+            };
+        }
+
+
+        private void GameOver()
+        {
+            gameTimer.Stop();
+            btnStart.Enabled = true;
+
+            if (_score > _highScore)
+            {
+                _highScore = _score;
+                lblHighScore.Text = $"High Score: {_highScore}";
+                lblHighScore.ForeColor = Color.Blue;
+            }
         }
     }
 }
